bdd1ad39652dc76d7d0dc5dc8d08789640b76762
Sourav Paul <souravpaul@protonmail.com>
2023-07-24 12:42:17 +0530

    Changed file source from files to in-memory map

--- CppRest Server/http_server.cpp
+++ CppRest Server/http_server.cpp
@@ -10,7 +10,153 @@
 #include <cstdio>
 #include <unistd.h>
 #include<time.h>
+#include <map>
+
 using namespace std;
+
+std::map<std::string, std::string> htmlMap = {
+      {"/", "<!DOCTYPE html> <html lang=\"en\">\
+              <head>\
+                <meta charset=\"UTF-8\">\
+                <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                <title>Document</title>\
+              </head>\
+              <body>\
+                <h1>This is BootCampus.</h1>\
+                <a href=\"apart1/index.html\">Apartment 1</a>\
+                <a href=\"apart2/index.html\">Apartment 2</a>\
+                <a href=\"apart3/index.html\">Apartment 3</a>\
+              </body>\
+              </html>"},
+      {"/index.html", "<!DOCTYPE html> <html lang=\"en\">\
+              <head>\
+                <meta charset=\"UTF-8\">\
+                <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                <title>Document</title>\
+              </head>\
+              <body>\
+                <h1>This is BootCampus.</h1>\
+                <a href=\"apart1/index.html\">Apartment 1</a>\
+                <a href=\"apart2/index.html\">Apartment 2</a>\
+                <a href=\"apart3/index.html\">Apartment 3</a>\
+              </body>\
+              </html>"},
+      {"/apart1/index.html", "<!DOCTYPE html>\
+                  <html lang=\"en\">\
+                  <head>\
+                    <meta charset=\"UTF-8\">\
+                    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                    <title>Apartment 1</title>\
+                  </head>\
+                  <body>\
+                    <h1> This is Apartment 1. Child of Society BootCampus.</h1>\
+                    <a href=\"flat11/index.html\">Flat 1</a>\
+                    <a href=\"flat12/index.html\">Flat 2</a>\
+                    <a href=\"../index.html\">GO to home</a>\
+                  </body>\
+                  </html>"},
+      {"/apart1/flat11/index.html", "<!DOCTYPE html>\
+                          <html lang=\"en\">\
+                          <head>\
+                              <meta charset=\"UTF-8\">\
+                              <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                              <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                              <title>Flat 11</title>\
+                          </head>\
+                          <body>\
+                              <h1>Welcome to 1st Flat of Apartment 1.</h1>\
+                              <a href=\"../../index.html\">GO to home</a>\
+                          </body>\
+                          </html>"},
+
+      {"/apart1/flat12/index.html", "<!DOCTYPE html>\
+                          <html lang=\"en\">\
+                          <head>\
+                              <meta charset=\"UTF-8\">\
+                              <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                              <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                              <title>Flat 12</title>\
+                          </head>\
+                          <body>\
+                              <h1>Welcome to 2nd Flat of Apartment 1.</h1>\
+                              <a href=\"../../index.html\">GO to home</a>\
+                          </body>\
+                          </html>"},
+      {"/apart2/index.html", "<!DOCTYPE html>\
+                  <html lang=\"en\">\
+                  <head>\
+                      <meta charset=\"UTF-8\">\
+                      <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                      <title>Apartment 2</title>\
+                  </head>\
+                  <body>\
+                      <h1>This is Apartment 2. Child of Society BootCampus.</h1>\
+                      <a href=\"flat21/index.html\">Flat 1st</a>\
+                      <a href=\"../index.html\">GO to home</a>\
+                  </body>\
+                  </html>"},
+      {"/apart2/flat21/index.html", "<!DOCTYPE html>\
+                                      <html lang=\"en\">\
+                                      <head>\
+                                          <meta charset=\"UTF-8\">\
+                                          <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                                          <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                                          <title>Document</title>\
+                                      </head>\
+                                      <body>\
+                                          <h1>Welcome to 1st Flat of Apartment 2.</h1>\
+                                          <a href=\"../../index.html\">GO to home</a>\
+                                      </body>\
+                                      </html>"},
+      {"/apart3/index.html", "<!DOCTYPE html>\
+                                <html lang=\"en\">\
+                                <head>\
+                                    <meta charset=\"UTF-8\">\
+                                    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                                    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                                    <title>Apartment 1</title>\
+                                </head>\
+                                <body>\
+                                    <h1>This is Apartment 3. Child of Society BootCampus.</h1>\
+                                    <a href=\"flat31/index.html\">Flat 1st</a>\
+                                    <a href=\"flat32/index.html\">Flat 2nd</a>\
+                                    <a href=\"../index.html\">GO to home</a>\
+                                </body>\
+                                </html>"},
+
+        {"/apart3/flat31/index.html", "<!DOCTYPE html>\
+                                        <html lang=\"en\">\
+                                        <head>\
+                                            <meta charset=\"UTF-8\">\
+                                            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                                            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                                            <title>Flat31</title>\
+                                        </head>\
+                                        <body>\
+                                            <h1>Welcome to 1st Flat of Apartment 3.</h1>\
+                                            <a href=\"../../index.html\">GO to home</a>\
+                                        </body>\
+                                        </html>"},
+
+        {"/apart3/flat32/index.html", "<!DOCTYPE html>\
+                                        <html lang=\"en\">\
+                                        <head>\
+                                            <meta charset=\"UTF-8\">\
+                                            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+                                            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+                                            <title>Flat 32</title>\
+                                        </head>\
+                                        <body>\
+                                            <h1>Welcome to 2nd Flat of Apartment 3.</h1>\
+                                            <a href=\"../../index.html\">GO to home</a>\
+                                        </body>\
+                                        </html>"},
+
+      };
 
 vector<string> split(const string &s, char delim) {
   vector<string> elems;
@@ -41,46 +187,24 @@ HTTP_Request::HTTP_Request(string request) {
 }
 
 HTTP_Response *handle_request(string path) {
-  //HTTP_Request *request = new HTTP_Request(req);
-
   HTTP_Response *response = new HTTP_Response();
-  char cwd[1024];
-
-  string url = getcwd(cwd, sizeof(cwd)) + string("/html_files") + path;
-  //cout<<"url is "<<url<<endl;
 
   response->HTTP_version = "1.0";
 
-  struct stat sb;
+  std::cout << path << std::endl;
 
-  if (stat(url.c_str(), &sb) == 0) // requested path exists
+  if (htmlMap.find(path) != htmlMap.end()) // requested path exists
   {
     response->status_code = "200";
     response->status_text = "OK";
     response->content_type = "text/html";
     string body;
 
-    if (S_ISDIR(sb.st_mode)) {
-      url=url+"/index.html";
-    }
-
-    string rdstring;
-    ifstream file (url);
-    if ( file.is_open() ) {
-    while ( file ) {
-      getline (file, rdstring);
-      body=body+rdstring;
-
-    }
-    
-    }else{
-      body="<html><h1>404 NOT FOUND</h1><html>";  // requested path does not exists
-    }
+    body = body + htmlMap[path];
 
     response->body = body;
     
   }
-
   else {
     response->status_code = "404";
     response->content_type = "text/html";
