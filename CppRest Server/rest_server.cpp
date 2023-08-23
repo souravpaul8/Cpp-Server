cb13e2bc2c3163685e92747171e042b664b56d90
Sourav Paul <souravpaul@protonmail.com>
2023-08-23 19:24:26 +0530

    Rest Server change to connect to Redis Server

--- CppRest Server/rest_server.cpp
+++ CppRest Server/rest_server.cpp
@@ -1,5 +1,8 @@
 #include <cpprest/http_listener.h>
 #include <cpprest/json.h>
+#include <cpprest/http_client.h>
+#include "cpprest/uri.h"
+#include <chrono>
 
 #include "http_server.hh"
 
@@ -7,23 +10,49 @@ using namespace web;
 using namespace web::http;
 using namespace web::http::experimental::listener;
 
-const utility::string_t SERVER_URL = U("http://localhost:8080");
+const int timeoutSeconds = 5;
+
+const utility::string_t SERVER_URL = U("http://localhost:80");
 
 void handle_get(const http_request& request)
 {
-    //std::cout << "Inside Handle Get" << std::endl;
-    http_response response;
+    http::uri redisServerUri = http::uri(U("http://localhost:5000"));
+    http::client::http_client redis(http::uri_builder(redisServerUri)
+                        .append_path(U("/"))
+                        .to_uri());
 
-    utility::string_t path = request.request_uri().path();
-    HTTP_Response *html_response = handle_request(path);
-    if(html_response->status_code == "200")
-        response.set_status_code(status_codes::OK);
-    else
-       response.set_status_code(status_codes::NotFound);
-    response.headers().add(U("Content-Type"), U("text/html"));
-    response.set_body(utility::conversions::to_string_t(html_response->body));
-    response.headers().add(U("Date"),html_response->time);
-    request.reply(response);
+    redis.request(methods::GET)
+    .then([=](pplx::task<http_response> task)
+    {
+        http_response resp;
+        try {
+            resp = task.get();
+        }catch(std::exception &e) {
+            std::cout << "EXCEPTION CAUGHT: " << e.what() << std::endl;
+        }
+
+        if(resp.status_code() == status_codes::OK){
+            resp.content_ready().then([=](web::http::http_response response) {
+                std::string htmlContent = response.to_string();
+                //std::cout << "HTML Response: " << htmlContent << std::endl;
+            });
+        } else {
+            std::cout << "HTTP Response Error: " << resp.status_code() << std::endl;
+        }
+        http_response response;
+
+        utility::string_t path = request.request_uri().path();
+
+        HTTP_Response *html_response = handle_request(path);
+        if(html_response->status_code == "200")
+            response.set_status_code(status_codes::OK);
+        else
+        response.set_status_code(status_codes::NotFound);
+        response.headers().add(U("Content-Type"), U("text/html"));
+        response.set_body(utility::conversions::to_string_t(html_response->body));
+        response.headers().add(U("Date"),html_response->time);
+        request.reply(response);
+    });
 }
 
 int main()
